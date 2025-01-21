934fb47b038cce878b5408a4bedee017a7252c98
Simon Holland Flarup <mail@simonflarup.dk>
2025-01-21 20:03:56 +0100

    Split up translations into namespaces

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -112,7 +112,7 @@ function DesktopNavigation(props: JSX.IntrinsicAttributes & React.ClassAttribute
 
 enum Language {
   English = "en",
-  Danish = "da"
+  Danish = "da-DK"
 }
 
 function LanguageDropdown() {
