68dcedc121870c7c969d701785e38b24f65e9608
Simon Holland Flarup <mail@simonflarup.dk>
2025-02-02 01:52:31 +0100

    Resize "Download" button

--- src/components/pages/index/Timeline.tsx
+++ src/components/pages/index/Timeline.tsx
@@ -32,11 +32,13 @@ export function Timeline(props: {icon: () => ReactNode, title: string, content: 
   const {t} = useTranslation()
   return (
     <div className="rounded-2xl border border-zinc-100 pt-6 px-6 pb-4 dark:border-zinc-700/40">
-      <h2 className="flex gap-4 items-center text-2xl font-semibold text-zinc-900 dark:text-zinc-100">
-        {props.icon()}
-        <span className="">{props.title}</span>
-        <Link className="ml-auto gap-2 flex flex-row items-center border-2 bg-zinc-100/5 dark:bg-zinc-900/20 border-zinc-100 dark:border-zinc-700/40 rounded-xl px-2 py-1 font-normal text-xl" to="/cv">
-          <CVIcon className="h-10 w-10 fill-zinc-200 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+      <h2 className="flex items-center text-2xl font-semibold text-zinc-900 dark:text-zinc-100">
+        <div className="flex gap-4 items-center">
+          {props.icon()}
+          <span className="">{props.title}</span>
+        </div>
+        <Link className="ml-auto gap-2 flex flex-row items-center border-2 bg-zinc-100/5 dark:bg-zinc-900/20 border-zinc-100 dark:border-zinc-700/40 rounded-xl px-4 py-1 font-normal text-lg" to="/cv">
+          <CVIcon className="h-8 w-8 fill-zinc-200 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
           {t("download_cv")}
         </Link>
       </h2>
